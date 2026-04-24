e38fe26f5f65fa85a1790665cc875f5c30b934c0
jy000n <jayoonchang@naver.com>
2026-04-24 22:25:46 +0900

    feat: GlobalExceptionHandler 및 Controller에 HTTP 상태 코드 적용

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -5,8 +5,9 @@ import org.sopt.dto.request.UpdatePostRequest;
 import org.sopt.dto.response.ApiResponse;
 import org.sopt.dto.response.CreatePostResponse;
 import org.sopt.dto.response.PostResponse;
-import org.sopt.exception.PostNotFoundException;
 import org.sopt.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,54 +22,64 @@ public class PostController {
     }
 
     @PostMapping
-    public ApiResponse<CreatePostResponse> createPost(
+    public ResponseEntity<ApiResponse<CreatePostResponse>> createPost(
             @RequestBody CreatePostRequest request
     ) {
-        return new ApiResponse<>(
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+            new ApiResponse<>(
                 true,
                 "게시글 생성 성공!",
                 postService.createPost(request)
+            )
         );
     }
 
     @GetMapping
-    public ApiResponse<List<PostResponse>> getAllPosts() {
-        return new ApiResponse<>(
+    public ResponseEntity<ApiResponse<List<PostResponse>>> getAllPosts() {
+        return ResponseEntity.ok(
+            new ApiResponse<>(
                 true,
                 "게시글 전체 조회 성공!",
                 postService.getAllPosts()
+            )
         );
     }
 
     @GetMapping("/{id}")
-    public ApiResponse<PostResponse> getPost(@PathVariable Long id) {
-        return new ApiResponse<>(
+    public ResponseEntity<ApiResponse<PostResponse>> getPost(@PathVariable Long id) {
+        return ResponseEntity.ok(
+            new ApiResponse<>(
                 true,
-                "게시글 조회 성공!",
+               "게시글 조회 성공!",
                 postService.getPost(id)
+            )
         );
     }
 
     @PatchMapping("/{id}")
-    public ApiResponse<Void> updatePost(@PathVariable Long id,
+    public ResponseEntity<ApiResponse<Void>> updatePost(@PathVariable Long id,
                                         @RequestBody UpdatePostRequest request) {
         postService.updatePost(id, request);
 
-        return new ApiResponse<>(
-                true,
-                "게시글 수정 성공!",
-                null
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+            true,
+            "게시글 수정 성공!",
+            null
+            )
         );
     }
 
     @DeleteMapping("/{id}")
-    public ApiResponse<Void> deletePost(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<Void>> deletePost(@PathVariable Long id) {
         postService.deletePost(id);
 
-        return new ApiResponse<>(
-                true,
-                "게시글 삭제 성공!",
-                null
+        return ResponseEntity.ok(
+            new ApiResponse<>(
+            true,
+            "게시글 삭제 성공!",
+            null
+            )
         );
     }
 }
--- src/main/java/org/sopt/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/exception/GlobalExceptionHandler.java
@@ -1,17 +1,44 @@
 package org.sopt.exception;
 
 import org.sopt.dto.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(PostNotFoundException.class)
-    public ApiResponse<Void> handlePostNotFound(PostNotFoundException e){
-        return new ApiResponse<>(
+    public ResponseEntity<ApiResponse<Void>> handlePostNotFound(PostNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new ApiResponse<>(
                 false,
                 e.getErrorCode() + ": " + e.getMessage(),
                 null
+                )
         );
     }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<Void>> handleIllegalArgumentException(IllegalArgumentException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                new ApiResponse<>(
+                        false,
+                        e.getMessage(),
+                        null
+                ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                new ApiResponse<>(
+                        false,
+                        "서버 내부 오류가 발생했습니다.",
+                        null
+                )
+        );
+    }
+
+
 }
